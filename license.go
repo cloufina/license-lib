bfde0ade457f86f478b02950fdf7988f3c1c4a39
Achmad Rifa'i <janoko.java22@yahoo.com>
2024-01-17 14:03:59 +0700

    if all doesn't match

--- license.go
+++ license.go
@@ -59,7 +59,7 @@ func checkingLicense(features []string) {
 	if data.exp.Before(time.Now()) {
 		panic(fmt.Errorf("expired license"))
 	}
-	if arrayutils.AnyOf(features, func(v string, index int) bool {
+	if arrayutils.AllOf(features, func(v string, index int) bool {
 		return !arrayutils.Contains(data.features, v)
 	}) {
 		panic(fmt.Errorf("invalid license"))
